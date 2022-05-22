c5f3f491cf72c66851d6dc79e721affda97e0ca4
Jahongir Mannonov <jahongir.sh.mannonov@gmail.com>
2022-05-22 13:56:05 +0500

    Initial commit

---
+++ app/src/main/java/uz/h1001/hadith/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.h1001.hadith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
