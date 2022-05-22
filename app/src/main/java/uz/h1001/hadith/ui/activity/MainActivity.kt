43a291fc56b53eb467b37d1cad670b150a638fda
Jahongir Mannonov <jahongir.sh.mannonov@gmail.com>
2022-05-22 14:15:32 +0500

    Navigation, appfont, style, colors added

--- app/src/main/java/uz/h1001/hadith/ui/activity/MainActivity.kt
+++ app/src/main/java/uz/h1001/hadith/ui/activity/MainActivity.kt
@@ -2,11 +2,15 @@ package uz.h1001.hadith.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowInsetsControllerCompat
 import uz.h1001.hadith.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowInsetsControllerCompat(window,window.decorView).isAppearanceLightStatusBars = true
+
     }
 }
